96d26966b717d81fcf127d831cda8293fce59858
deter0 <deter0109@gmail.com>
2021-11-12 16:33:38 -0500

    Album Styling

--- ReactApp/app/src/Mp3Player/Pages/Album.tsx
+++ ReactApp/app/src/Mp3Player/Pages/Album.tsx
@@ -27,10 +27,20 @@ export default class Album extends Component {
 	}
 	render() {
 		return (
-			<div className="page-padding">
-				<img src={`http://localhost:${App.Port[0]}${this.state.Album?.Cover}`} alt="album-cover" />
-				<h1>{this.state.Album?.Title}</h1>
-				<VerticalSongs NoPages={true} Items={this.state.Album ? this.state.Album.Songs : []} />
+			<div className="page-padding-top">
+				<div className="info-container">
+					<img draggable={false} className="album-img" src={`http://localhost:${App.Port[0]}${this.state.Album?.Cover}`} alt="album-cover" />
+					<div className="album-info">
+						<h1 className="album-title">{this.state.Album?.Title}</h1>
+						<h1 className="album-artist"><span className="material-icons">person</span>{this.state.Album?.Artist}</h1>
+						<div className="button-container">
+							<button className="button-highlight">Add to library</button>
+							<button>Delete</button>
+						</div>
+					</div >
+				</div>
+				<h1 className="songs-text">Songs</h1>
+				<VerticalSongs style={{ paddingTop: 0 }} NoPages={true} Items={this.state.Album ? this.state.Album.Songs : []} />
 			</div>
 		)
 	}
