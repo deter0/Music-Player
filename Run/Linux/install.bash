3848c23e032fb2b76dee6947982f2523b01df21d
deter0 <deter0109@gmail.com>
2021-11-12 21:15:43 -0500

    Run and install scripts

---
+++ Run/Linux/install.bash
@@ -0,0 +1,10 @@
+cd ../../
+cd ReactApp/app
+npm install
+cd ../../Server
+npm install
+cd ../SpotifyDownloader
+pip install -r requirements.txt
+cd ../Client
+npm install
+echo Done installing!
---
+++ Run/Linux/run.bash
@@ -0,0 +1,2 @@
+cd ../../
+(cd ReactApp/app/ && npm start) & (cd Server && npm start) && fg
