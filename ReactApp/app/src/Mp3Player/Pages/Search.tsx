f56e32caf5514f2a9699b56f7ebccbfb940ab8e8
deter0 <deter0109@gmail.com>
2021-11-14 08:16:27 -0500

    Search query caching

--- ReactApp/app/src/Mp3Player/Pages/Search.tsx
+++ ReactApp/app/src/Mp3Player/Pages/Search.tsx
@@ -3,34 +3,43 @@ import React, { Component } from 'react'
 import VerticalSongs from '../Components/VerticalSongs';
 import * as Types from "../Types";
 
+let CachedQuery = "";
 export default class Search extends Component {
 	state: { Songs: Types.Song[], Albums: Types.Album[] } = { Songs: [], Albums: [] };
+	componentDidMount() {
+		this.Search(CachedQuery);
+
+		const Search = document.getElementById("search-page-search");
+		if (Search) {
+			(Search as HTMLInputElement).value = CachedQuery;
+		}
+	}
 	OnInput(Event: React.FormEvent<HTMLInputElement>) {
 		let Query = (Event.target as HTMLInputElement).value;
+		this.Search(Query);
+		CachedQuery = Query;
+	}
+	Search(Query: string) {
 		window.API.post("/search/songs", undefined, {
 			params: {
 				Query: Query.trim()
 			}
 		}).then(Response => {
-			if (Query === (Event.target as HTMLInputElement).value) {
-				this.setState({ Songs: Response.data });
-			}
+			this.setState({ Songs: Response.data });
 		});
 		window.API.post("/search/albums", undefined, {
 			params: {
-				Query: (Event.target as HTMLInputElement).value.trim()
+				Query: Query.trim()
 			}
 		}).then(Response => {
-			if (Query === (Event.target as HTMLInputElement).value) {
-				this.setState({ Albums: Response.data });
-			}
+			this.setState({ Albums: Response.data });
 		});
 	}
 	render() {
 		return (
 			<div className="page-padding">
 				<h1>Search your music</h1>
-				<div className="search"><input placeholder="Search..." onInput={(Event) => this.OnInput(Event)} /></div>
+				<div className="search"><input id="search-page-search" placeholder="Search..." onInput={(Event) => this.OnInput(Event)} /></div>
 				<HorizontalScroller style={{ padding: 0 }} Items={this.state.Albums} />
 				<VerticalSongs songStyle={{ marginLeft: -15, width: `calc(100% + 5px)` }} style={{ padding: 0 }} Items={this.state.Songs} />
 			</div>
